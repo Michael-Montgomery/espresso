d0c7a409cd164bb3595cf4eebd3e330bb48b681b
Michael Montgomery <michaelmontgomery@HW-GPI-F0Q6WQJ7.local>
2026-03-01 21:18:00 -0500

    added cart image.

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,6 @@
 import Header from '../../components/header/header';
 import './home.css';
+import  cartImg  from '../../assets/cart.png';
 
 
 export default function Home() {
@@ -13,9 +14,10 @@ export default function Home() {
                     <div id="cloud">
                     </div>
                 </div>
+                <img id='homepage-coffee-cart' src={cartImg} alt='cart'></img>
 
                 <p id='homepage-address'>775 Malabar Rd. Palm Bay, FL</p>
-                <p id='credit-link'><a href='https://michael-montgomery.github.io/theory23'>T23</a></p>
+                <p id='credit-link'><a href='https://michael-montgomery.github.io/theory23'>MM</a></p>
             </div>
         </>
     )
